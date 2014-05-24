7192e4683a73cda56bd0e83d3ae1e1ff84b699b3
albertstepanyan <albstepanyan@gmail.com>
2014-05-24 19:41:59 +0200

    Foundation Based Skeleton

--- Objective-C-for-dummies/main.m
+++ Objective-C-for-dummies/main.m
@@ -12,9 +12,8 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Main called...");
+        // Initializing the App Bootstrap...
         
     }
     return 0;
